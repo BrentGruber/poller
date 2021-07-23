9da36f6e2958b33e71350b206a24e8f743dabb7f
Brent Gruber <brent.gruber77@gmail.com>
2021-07-22 20:37:00 -0400

    render options in poll_view

--- lib/poller_web/controllers/option_controller.ex
+++ lib/poller_web/controllers/option_controller.ex
@@ -17,7 +17,7 @@ defmodule PollerWeb.OptionController do
     with {:ok, %Option{} = option} <- Polls.create_option(poll, option_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.poll_option_path(conn, :show, option))
+      |> put_resp_header("location", Routes.poll_option_path(conn, :show, poll_id, option))
       |> render("show.json", option: option)
     end
   end
